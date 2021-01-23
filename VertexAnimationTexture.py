942e0048df56049017e7512bb23db3535224a7bf
salt-k2t <ys.yrkt@gmail.com>
2021-01-23 22:43:16 +0900

    regist menu

--- VertexAnimationTexture.py
+++ VertexAnimationTexture.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
+import maya.cmds as cmds
+import maya.mel as mel
 import maya.OpenMayaMPx
 
 import VertexAnimationTextureMainWindow as Vat
@@ -12,17 +14,26 @@ class VertexAnimationTexture(maya.OpenMayaMPx.MPxCommand):
         maya.OpenMayaMPx.MPxCommand.__init__(self)
 
     def doIt(self, args):
-        Vat.VertexAnimationTextureMainWindow()
+        openMainWindow()
 
 
 def creator():
     return VertexAnimationTexture()
 
 
+def openMainWindow(*args):
+    Vat.VertexAnimationTextureMainWindow()
+
+
 def initializePlugin(mObject):
     mPlugin = maya.OpenMayaMPx.MFnPlugin(mObject)
     try:
         mPlugin.registerCommand(kPluginCmdName, creator)
+        gMainWindow = mel.eval('$gmw = $gMainWindow')
+        menu = cmds.menu(parent=gMainWindow, tearOff=True,
+                         label=kPluginCmdName)
+        cmds.menuItem(parent=menu, label=kPluginCmdName,
+                      command=openMainWindow)
     except:
         sys.stderr.write('Failed load plugin : %s' % kPluginCmdName)
         raise
